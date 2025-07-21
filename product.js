b6e260eab49cdf36fb631e72a8cbdbe2af041efd
Bharanikumar-BK <bharanikumar2911@yahoo.com>
2025-07-21 11:16:20 +0530

    Functionality added

---
+++ product.js
@@ -0,0 +1,30 @@
+// Selecting side nav bar, menu icon
+
+var sideNav = document.getElementById("sideNav");
+var menuIcon = document.getElementById("menuIcon");
+var closeNav = document.getElementById("closeNav")
+
+menuIcon.addEventListener("click", function(){
+    sideNav.style.right= 0;
+})
+closeNav.addEventListener("click",function(){
+    sideNav.style.right = "-50%"
+})
+
+//product search functionality
+var productContainer = document.getElementById("product-container");
+var search = document.getElementById("search");
+var productlist = productContainer.querySelectorAll("div");
+
+    search.addEventListener("keyup", function() {
+        var enteredValue = event.target.value.toUpperCase();
+
+        for (count = 0; count < productlist.length; count = count + 1) {
+            var productName = productlist[count].querySelector("h1").textContent;
+            if (productName.toUpperCase().indexOf(enteredValue) < 0) {
+                productlist[count].style.display = "none";
+            } else {
+                productlist[count].style.display = "block";
+            }
+        }
+    });
